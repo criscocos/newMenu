02cfa8e43e6826a4a41988bd2cc750d4f547570a
bpas247 <bpas247@hotmail.com>
2016-05-11 13:00:50 -0700

    revised the practice page. (please read desc.)
    Now instead of calling different things and creating 3 div tags, all you
    have to do is link the references and call the master function and
    everything else will be taken care of

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,4 @@
+//Will set the current version 
 function setVersion() {
     var width = $(window).width();
     if(width > 469) {
@@ -23,4 +24,18 @@ function setVersion() {
                 $("#mheader").show();
             }
         });
+}
+
+
+//call this function everytime for every webpage (Do NOT call the above function. It is included in this function) 
+function masterFunction() {
+    $('body').prepend("<div id='header'></div>");
+    $('body').prepend("<div id='mheader'></div>");
+    $('body').append("<div id='footer'></div>");
+    
+    $("#mheader").load('mtm/master-header-mobile.html');
+    $("#header").load('mtm/master-header.html');
+    $("#footer").load('mtm/master-footer.html');
+    
+    setVersion();
 }
